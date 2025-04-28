fd0257999b23765e0d9744509159e2cfdb440588
Parikshit <Parikshitmali17@gmail.com>
2025-04-28 22:57:03 +0530

    done

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,10 @@
 // write code for Welcome component here
+import React from 'react'
+
+export default function Welcome({name}) {
+  return (
+    <div><h1>Hey !{name}</h1>
+    <p>Welcome to School.</p>
+    </div>
+  )
+}
